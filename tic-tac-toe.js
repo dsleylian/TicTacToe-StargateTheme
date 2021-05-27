84f9d2d310f1c4e7414a023a9c769f8a082a7b23
dsleylian <daryaleylian@gmail.com>
2021-05-27 12:20:13 -0700

    added comments to JS file, IE-Edge meta to html

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -20,9 +20,12 @@ let torchOnO = document.createElement("img");
 torchOnO.src = "images/flameon.gif";
 torchOnO.setAttribute("id", "lefttorch");
 
+//not sure if will use torch off
 //let torchOff = document.createElement("img");
 //torchOff.src = "images/flameoff.gif";
 
+
+//create left & right side displays for players turn plus torch
 let playerDisplayX = document.createElement("p");
 playerDisplayX.setAttribute("class", "right");
 let textDisplayX = document.createTextNode(`Player X's turn`);
@@ -43,9 +46,12 @@ let holdTheTorchO = document.createElement("div");
 playerDisplayO.appendChild(holdTheTorchO);
 holdTheTorchO.appendChild(torchOnO);
 
+
+//set up reset button and call the resetGame function when clicked
 let resetButton = document.querySelector("#startover");
 resetButton.addEventListener("click", resetGame);
 
+// create winner/no winner banners 
 let xWinner = document.createElement("div");
 let xWinnerPara = document.createElement("p");
 let xWinnerText = document.createTextNode("Player X Won!!!");
@@ -73,6 +79,7 @@ noWinnerPara.appendChild(noWinnerText);
 document.body.insertBefore(noWinner, resetButton);
 noWinner.style.visibility = "hidden";
 
+//2 set up functions depensing on who starts
 
 function setUpX () {
 
@@ -80,7 +87,6 @@ choosePlayer.style.visibility = "hidden";
 playerDisplayX.style.visibility = "visible";
 currentPlayer = "x";
 startGame();
-
 }
 
 function setUpO () {
@@ -98,8 +104,11 @@ function startGame () {
     })
 }
 
+//marking the square when it's clicked
+
 function markTheSquare(e) {
 
+        // make an a
     let divArray = Array.from(insideDivs);
     let arrayIndex = divArray.indexOf(e.target);
 
@@ -115,7 +124,10 @@ function markTheSquare(e) {
         playerDisplayX.style.visibility = "hidden";
     }
 
+    //remove the event listener from the square that was clicked
     divArray[arrayIndex].removeEventListener("click", markTheSquare);
+
+    //each time a button is clicked, check for these conditions to see if a player won or if it is a tie and all squares are marked
 
     if (   ( (insideDivs[0].classList.value) === "markx" && (insideDivs[1].classList.value) === "markx" && (insideDivs[2].classList.value) === "markx" ) ||
     ( (insideDivs[3].classList.value) === "markx" && (insideDivs[4].classList.value) === "markx" && (insideDivs[5].classList.value) === "markx" ) ||
@@ -126,7 +138,8 @@ function markTheSquare(e) {
     ( (insideDivs[0].classList.value) === "markx" && (insideDivs[4].classList.value) === "markx" && (insideDivs[8].classList.value) === "markx" ) ||
     ( (insideDivs[2].classList.value) === "markx" && (insideDivs[4].classList.value) === "markx" && (insideDivs[6].classList.value) === "markx" ) )  {
 
-    xWinner.style.visibility = "visible";
+    xWinner.style.visibility = "visible";  //if x won display x winner banner 
+
     }else if (   ( (insideDivs[0].classList.value) === "marko" && (insideDivs[1].classList.value) === "marko" && (insideDivs[2].classList.value) === "marko" ) ||
     ( (insideDivs[3].classList.value) === "marko" && (insideDivs[4].classList.value) === "marko" && (insideDivs[5].classList.value) === "marko" ) ||
     ( (insideDivs[6].classList.value) === "marko" && (insideDivs[7].classList.value) === "marko" && (insideDivs[8].classList.value) === "marko" ) ||
@@ -136,30 +149,34 @@ function markTheSquare(e) {
     ( (insideDivs[0].classList.value) === "marko" && (insideDivs[4].classList.value) === "marko" && (insideDivs[8].classList.value) === "marko" ) ||
     ( (insideDivs[2].classList.value) === "marko" && (insideDivs[4].classList.value) === "marko" && (insideDivs[6].classList.value) === "marko" ) )  {
 
-    oWinner.style.visibility = "visible";
+    oWinner.style.visibility = "visible"; //if O won, display O winner banner 
 
     } else if ( ((insideDivs[0].classList.value) !== "inside" ) && ((insideDivs[1 ].classList.value) !== "inside" ) && ((insideDivs[2].classList.value) !== "inside" ) &&
     ((insideDivs[3].classList.value) !== "inside" ) && ((insideDivs[4].classList.value) !== "inside" ) && ((insideDivs[5].classList.value) !== "inside" ) &&
     ((insideDivs[6].classList.value) !== "inside" ) && ((insideDivs[7].classList.value) !== "inside" ) && ((insideDivs[8].classList.value) !== "inside" )
     ) {
-        noWinner.style.visibility = "visible";
+        noWinner.style.visibility = "visible";  //if all squares are clicked but no winners, display not winner banner 
     }
 
 }
 
 function resetGame() {
 
-    insideDivs.forEach( div => {
+    //remove all event listeners from unclclicked squares 
+        insideDivs.forEach( div => {
         if (div.classList.value === "inside"){
             div.removeEventListener("click", markTheSquare);
         }
     })
 
+    //reset classes of all squares to "inside" to remove x/o markings
     insideDivs.forEach( div => {
         div.setAttribute("class", "inside");
     })
+    //hide banners, hide current player, show the "choose player" element at top
     playerDisplayO.style.visibility = "hidden";
     playerDisplayX.style.visibility = "hidden";
+    
     choosePlayer.style.visibility = "visible";
 
     oWinner.style.visibility = "hidden";
